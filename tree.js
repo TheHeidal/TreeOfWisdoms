5b1f9b7151253044887027e65275b0c6fe4dea3e
Mars Heidal <theHeidal@gmail.com>
2024-04-20 19:47:06 -0700

    now can make cards, aborted attempt to scrape images but I think the rowenarium will just have to deal with it.

--- tree.js
+++ tree.js
@@ -1,3 +1,5 @@
+const imgURLPrefix = 'https://uadaf.theevilroot.xyz/rowenarium/static/bhimages';
+
 class Skill {
     //constructor
     constructor(jsonSkill) {
@@ -5,23 +7,101 @@ class Skill {
         this.label = jsonSkill.Label;
         this.desc = jsonSkill.Desc;
         this.aspects = jsonSkill.aspects; //Objec
-        console.log(Object.keys(this.aspects));
         this.arts = Object.keys(this.aspects).filter(aspect => aspect.startsWith('w.'))
     }
+
+    makeCard() {
+        console.log('making %s', this.id)
+        // Card Div
+        const card = document.createElement("div");
+        card.className = "skill card";
+        card.id = this.id;
+        document.getElementById("skills").append(card);
+        // Top Div
+        const images = document.createElement("div");
+        card.append(images);
+        // Arts symbols Div
+        const arts = document.createElement("div"); arts.className = "arts";
+        images.append(arts);
+
+        // Arts Imgs
+        console.assert((this.arts.length === 2), 'wrong number of arts!', this);
+        function makeArtsImg(aspectStr) {
+            const art = document.createElement("img");
+            art.attributes['src'] = `${imgURLPrefix}/aspects/${aspectStr}.png`;
+            art.attributes['alt'] = aspectStr.slice(2); //chop off 'w.'
+            return art;
+        };
+        const art0 = makeArtsImg(this.arts[0]);
+        const art1 = makeArtsImg(this.arts[1]);
+        arts.append(art0, art1);
+        //
+        const skillImg = document.createElement("img");
+        skillImg.attributes["src"] = `${imgURLPrefix}/elements/${this.id}.png`;
+    }
+
 }
 
+function downloadImages(imageUrls) {
+    // Iterate through the array of image URLs
+    imageUrls.forEach(function (url) {
+        // Fetch the image
+        fetch(url)
+            .then(response => response.blob()) // Convert response to Blob
+            .then(blob => {
+                // Create a URL object
+                const imgUrl = URL.createObjectURL(blob);
 
-const SKILLS_URL = 'https://raw.githubusercontent.com/TheHeidal/TreeOfWisdoms/main/skills.json';
-const SKILLS_FULL = fetch(SKILLS_URL)
-    .then(response => response.json())
-    .then(jsonData => {
-        // console.log(jsonData.elements);
-        var skills = new Object();
-        for (var jsonSkill of jsonData.elements) {
-            console.log(jsonSkill);
-            skills[jsonSkill.id] = new Skill(jsonSkill);
-            console.log(skills[jsonSkill.id]);
-        }
-        // console.log(skills);
-    })
-    .catch(err => console.error('Error fetching JSON:', err));
+                // Create an anchor element
+                const link = document.createElement('a');
+                link.href = imgUrl;
+
+                // Set the download attribute to force download
+                link.setAttribute('download', '');
+
+                // Simulate a click to trigger download
+                document.body.appendChild(link);
+                link.click();
+
+                // Clean up: remove the URL object and anchor element
+                URL.revokeObjectURL(imgUrl);
+                document.body.removeChild(link);
+            })
+            .catch(error => {
+                console.error('Error downloading image:', error);
+            });
+    });
+}
+
+function generateSkills() {
+    const SKILLS_URL = 'https://raw.githubusercontent.com/TheHeidal/TreeOfWisdoms/main/skills.json';
+    const SKILLS_FULL = fetch(SKILLS_URL)
+        // de-JSON
+        .then(response => response.json())
+        // create map of skills
+        .then(jsonData => {
+            // console.log(jsonData.elements);
+            var skills = new Map();
+            for (var jsonSkill of jsonData.elements) {
+                skills[jsonSkill.id] = new Skill(jsonSkill);
+                skills[jsonSkill.id].makeCard();
+            }
+            return skills;
+        })
+        // .then(skills => {
+        //     var ids = Object.keys(skills);
+        //     // console.log(ids);
+        //     var urlStrings = ids.map(skill => `${imgURLPrefix}/elements/${skill}.png`);
+        //     // console.log(urlStrings);
+        //     var realURLS = urlStrings.map(url => new URL(url));
+        //     console.log(realURLS);
+        // })
+        // .then(urls => downloadImages(urls))
+        .catch(err => console.error('Error fetching JSON:', err));
+    return SKILLS_FULL;
+};
+
+document.body.onload = generateSkills();
+
+
+// downloadImages(imageUrls);
